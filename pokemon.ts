510468558564654ffa22924dfed3a0d96e91716b
alanraso <alan.raso@taqtile.com.br>
2019-10-25 16:08:07 -0300

    Remove features

--- pokemon.ts
+++ pokemon.ts
@@ -1,12 +1,4 @@
 export class Pokemon {
   name: string;
   hitPoints: number;
-
-  move() {
-    console.log(this.name + ' moved!');
-  }
-
-  attack() {
-    console.log(this.name + ' attacked!');
-  }
 }
