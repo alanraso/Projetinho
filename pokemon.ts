21d56fa835de7686e00c198ef67f191aa737150c
alanraso <alan.raso@taqtile.com.br>
2019-10-25 16:43:10 -0300

    Pokemon attack

--- pokemon.ts
+++ pokemon.ts
@@ -1,8 +1,12 @@
 export class Pokemon {
   name: string;
   hitPoints: number;
-
+  
   move() {
     console.info(`${this.name} moved!`);
+  }
+
+  attack() {
+    console.info(`${this.name} attacked!`);
   }
 }
