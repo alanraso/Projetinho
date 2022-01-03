3d86b06873398e3e9daa1f1d90e8b7ba867c0e6c
Matheus Felix <matheus.felix@taqtile.com.br>
2022-01-03 17:04:44 -0300

    feat: add hp

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Pokemon } from './pokemon';
 
 const pokemon = new Pokemon();
 pokemon.name = 'Pikachu';
+pokemon.hitpoints = 200;
 
 console.log(pokemon);
--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,3 +1,4 @@
 export class Pokemon {
   name: string;
+  hitpoints: number;
 }
