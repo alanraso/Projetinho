d0761acaf05236c179a2e982b8c752a1d543c183
alanraso <alan.raso@taqtile.com.br>
2022-09-01 14:59:13 -0300

    Correcting pikachu hp

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { Pokemon } from './pokemon';
 const pokemon = new Pokemon();
 pokemon.name = 'Pikachu';
 pokemon.level = 10;
-pokemon.hitpoints = 25;
+pokemon.hitpoints = 28;
 
 console.log(pokemon);
