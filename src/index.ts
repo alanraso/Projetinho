b27b7e0ff8318e20894c656e2fad90bc51f919d9
alanraso <alan.raso@taqtile.com.br>
2023-05-19 14:55:59 -0300

    Adds types on Pokemon

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { Pokemon } from './pokemon';
 const pokemon = new Pokemon();
 pokemon.name = 'Pikachu';
 pokemon.hitpoints = 40;
+pokemon.types = ['Electric'];
 
 pokemon.attack();
 pokemon.move();
+
+console.log(pokemon.getTypes());
--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,6 +1,11 @@
 export class Pokemon {
   name: string;
   hitpoints: number;
+  types: string[];
+
+  getTypes(): string[] {
+    return this.types;
+  }
 
   attack() {
     console.log(`${this.name} attacked!`);
