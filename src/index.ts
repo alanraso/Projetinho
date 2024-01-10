5e421c11e66271e807add8dfa77f42da327c4e14
Alan Raso <alan.raso@taqtile.com.br>
2024-01-10 13:52:13 -0300

    Updates libs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Pokemon } from '#root/pokemon.js';
 
 const bulbasaur = new Pokemon();
 bulbasaur.name = 'Bulbasaur';
-bulbasaur.hitpoints = 22;
+bulbasaur.hitpoints = 40;
 
 console.log(bulbasaur);
